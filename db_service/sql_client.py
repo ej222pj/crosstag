68312642637a9bd3dbabf3097b14209786c7aa81
Kristoffer Svensson <kristoffer@KS.crosstag>
2016-05-02 13:25:36 +0200

    added: function for registration to sql db

--- db_service/sql_client.py
+++ db_service/sql_client.py
@@ -5,21 +5,32 @@ from db_service import sql_client_cfg as cfg
 class SqlClient():
 
     def __init__(self, username=cfg.USERNAME, password=cfg.PASSWORD):
-        self.dbDriver = 'Driver={SQL Server};'
+        self.dbDriver = 'Driver={FreeTDS};'
         self.dbServer = 'Server=' + cfg.SERVER + ';'
         self.dbDatabase = 'Database=' + cfg.DATABASE + ';'
         self.dbUsername = 'uid=' + username + ';'
         self.dbPassword = 'pwd=' + password
 
-    def print_this_shit(self):
-        print(self.dbDriver, self.dbServer, self.dbDatabase, self.dbUsername, self.dbPassword)
+    def get_connection_string(self):
+        return self.dbDriver + self.dbServer + self.dbDatabase + self.dbUsername + self.dbPassword
+
+    def do_registration(self, tenant):
+            connection_string = self.get_connection_string()
+            try:
+                my_connection = pypyodbc.connect(connection_string)
+                cursor = my_connection.cursor()
+
+                cursor.execute('exec CreateUserLogin(?,?,?,?,?,?,?,?,?,?)',
+                (tenant[0], tenant[1], tenant[2], tenant[3],tenant[4], tenant[5], tenant[6], tenant[7], tenant[8], tenant[9]))
+
+            except pypyodbc.DatabaseError as error:
+                print(error.value)
+                print('FATALITY')
 
     def try_connection(self):
         print('try connection')
         try:
             connString = self.dbDriver + self.dbServer + self.dbDatabase + self.dbUsername + self.dbPassword
-            print(connString)
-            print('hej')
             myConnection = pypyodbc.connect(connString)
             print('Connection works!!!')
         except pypyodbc.DatabaseError as error:
