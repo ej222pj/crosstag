8d26818c3c3f2f74cdde484ba5839b32015be490
Kristoffer Svensson <kristoffer@KS.crosstag>
2016-05-10 14:48:32 +0200

    added: travis env variables

--- db_service/sql_client_cfg.py
+++ db_service/sql_client_cfg.py
@@ -1,4 +1,8 @@
-SERVER = ''
-DATABASE = ''
-USERNAME = ''
-PASSWORD = ''
+import os
+SERVER = os.environ['database_server']
+DATABASE = os.environ['database_name']
+USERNAME = os.environ['database_username']
+PASSWORD = os.environ['database_password']
+TENANT_PASSWORD = os.environ['database_tenantpw']
+
+
