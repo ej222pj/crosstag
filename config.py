24ef92f309a5eb9f749904a2d83abc9ef0d1c11b
Eric <ej222pj@student.lnu.se>
2016-05-18 15:39:42 +0200

    removed from config

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
 DEBUG = True
 PORT = 80
-SQLALCHEMY_DATABASE_URI = "sqlite:///dev_db.db"
 SECRET_KEY = "AukhdaOha/22GissaDennaDA"
-recipients = ['test@example.se', 'test@example.se']
-sender = "system@example.se"
