be2e16bae428b7b77be9be5cb7c508df7d99a891
Eric <ej222pj@student.lnu.se>
2016-05-19 12:56:11 +0200

    added comments

--- db_service/debt_sql_client.py
+++ db_service/debt_sql_client.py
@@ -6,7 +6,19 @@ from flask import session
 
 
 class DebtSqlClient:
+    """
+    Class to Connect to the DB an /Create/Read/Delete a Debt
+
+    __init__ - Creates connection string variables
+    get_connection_string - Creates connection string from the variables
+    get_debt - Get all Debts or the Debt of a specific User.
+    add_dept - Add a new Debt for a User
+    remove_debt - Remove 1 specific Debt
+    """
     def __init__(self):
+        """
+        Init function that creates the variables for the connection string
+        """
         self.dbDriver = 'Driver={FreeTDS};'
         self.dbServer = 'Server=' + cfg.SERVER + ';'
         self.dbDatabase = 'Database=' + cfg.DATABASE + ';'
@@ -14,9 +26,22 @@ class DebtSqlClient:
         self.dbPassword = 'pwd=' + cfg.TENANT_PASSWORD + session['username']
 
     def get_connection_string(self):
+        """
+        Creates a connection string for the DB from the variables created in init.
+
+        :return: Connection String
+        """
         return self.dbDriver + self.dbServer + self.dbDatabase + self.dbUsername + self.dbPassword
 
     def get_debt(self, user_id=0):
+        """
+        Takes an user_id as argument and get Debts depending on the user_id.
+        If user_id is 0, get all. If user_id is > 0, get specific.
+
+        :param user_id: Id of an user.
+        :type user_id: integer
+        :return: Representation of the class Debt
+        """
         connection_string = self.get_connection_string()
         try:
             my_connection = pypyodbc.connect(connection_string)
@@ -28,6 +53,7 @@ class DebtSqlClient:
             cursor.close()
             my_connection.close()
             return_array = []
+            # If the debts are for a specific user. Create a custom array.
             if user_id is not 0:
                 for debt in value:
                     return_array.append(sql_debt.SQLDebt(debt[1], debt[4], debt[2], debt[3][:-11], debt[0]))
@@ -46,6 +72,13 @@ class DebtSqlClient:
             print(error.value)
 
     def add_dept(self, debt):
+        """
+        Takes Debt as argument and then creates a new Debt.
+
+        :param debt: A debt representation
+        :type debt: debt class
+        :return: true
+        """
         connection_string = self.get_connection_string()
         try:
             my_connection = pypyodbc.connect(connection_string)
@@ -62,6 +95,13 @@ class DebtSqlClient:
             print(error.value)
 
     def remove_debt(self, debt_id):
+        """
+        Takes debt_id as argument and then removes a Debt.
+
+        :param debt_id: Id of a debt
+        :type debt_id: integer
+        :return: true
+        """
         connection_string = self.get_connection_string()
         try:
             my_connection = pypyodbc.connect(connection_string)
--- db_service/register_login_sql_client.py
+++ db_service/register_login_sql_client.py
@@ -4,7 +4,18 @@ from datetime import datetime, timedelta
 
 
 class RegisterLoginSqlClient:
+    """
+    Class to Connect to the DB an /Create/Read/Delete a Debt
+
+    __init__ - Creates connection string variables
+    do_registration - Creates a new Tenant in the DB when a Tenant tries to register
+    do_login - Tries to login the Tenant
+    get_tenant_with_api_key - Checks if a Tenant exists when using the rest API
+    """
     def __init__(self, username=cfg.USERNAME, password=cfg.PASSWORD):
+        """
+        Init function that creates the variables for the connection string
+        """
         self.dbDriver = 'Driver={FreeTDS};'
         self.dbServer = 'Server=' + cfg.SERVER + ';'
         self.dbDatabase = 'Database=' + cfg.DATABASE + ';'
@@ -12,9 +23,21 @@ class RegisterLoginSqlClient:
         self.dbPassword = 'pwd=' + password
 
     def get_connection_string(self):
+        """
+        Creates a connection string for the DB from the variables created in init.
+
+        :return: Connection String
+        """
         return self.dbDriver + self.dbServer + self.dbDatabase + self.dbUsername + self.dbPassword
 
     def do_registration(self, tenant):
+        """
+        Takes a Tenant as argument and register it.
+
+        :param tenant: A Tenant representation
+        :type tenant: tenant class
+        :return: True
+        """
         connection_string = self.get_connection_string()
         try:
             my_connection = pypyodbc.connect(connection_string)
@@ -24,16 +47,30 @@ class RegisterLoginSqlClient:
                            tenant['active_fortnox'] + "','" + tenant['gym_name'] + "','" + tenant['address'] + "','" +
                            tenant['phone'] + "','" + tenant['zip_code'] + "','" + tenant['city'] + "','" +
                            tenant['email'] + "','" + tenant['pass'] + "' )}")
+            value = cursor.fetchall()[0]
+
             cursor.commit()
             cursor.close()
             my_connection.close()
-            return True
+            
+            if value[0] is None:
+                return False
+            else:
+                return True
 
         except pypyodbc.DatabaseError as error:
             print(error.value)
             print('FATALITY')
 
     def do_login(self, username):
+        """
+        Takes a username and checks if it exists in the DB.
+        Gives back the Tenants password to check if it matches input password.
+
+        :param username: A Tenant representation
+        :type username: tenant class
+        :return: Tenant password
+        """
         connection_string = self.get_connection_string()
         try:
             my_connection = pypyodbc.connect(connection_string)
