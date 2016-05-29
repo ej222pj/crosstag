b30481e0b098e811f51ddca03cf5ddb414584299
Eric Sjöström Jennerstrand <ej222pj@student.lnu.se>
2016-05-29 18:05:39 +0200

    Added werkzeug, added correct sql client file

--- db_service/sql_client_cfg.py
+++ db_service/sql_client_cfg.py
@@ -1,7 +1,5 @@
-import os
-SERVER = os.environ['database_server']
-DATABASE = os.environ['database_name']
-USERNAME = os.environ['database_username']
-PASSWORD = os.environ['database_password']
-TENANT_PASSWORD = os.environ['database_tenantpw']
-
+SERVER = ''
+DATABASE = ''
+USERNAME = ''
+PASSWORD = ''
+TENANT_PASSWORD = ''
